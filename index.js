8c434e0f3bccb2142a5fc0cf62342eb6881547c5
Sebastián Vázquez <88560731+sebavazque@users.noreply.github.com>
2023-02-09 14:31:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(
   path.join(__dirname,"./public")));
 
   mercadopago.configure({
-  access_token: 'TEST-4427981030188178-020410-f2c190606d5552ae69cfbcbb4dfce592-442830367'})
+  access_token: 'APP_USR-6623451607855904-111502-1f258ab308efb0fb26345a2912a3cfa5-672708410'})
 
           //routes
           app.post('/payment', (req, res) => {
