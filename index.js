5ae14d36d7ed5ed4d2409aa43b6b26c23fe095ea
Sebastián Vázquez <88560731+sebavazque@users.noreply.github.com>
2023-02-09 11:38:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ app.use(express.static(
             });
           })
 
-    const PORT = process.env.PORT || 3001;
+    
 
 
 
-    app.listen(PORT, () => {
-      console.log(`Server listening on ${PORT}`);
+    app.listen(3001, () => {
+      console.log(`Server listening on 3001`);
     });
