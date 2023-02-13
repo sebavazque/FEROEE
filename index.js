1d62605986b991edd34f01f3ec1e73d0e35971d3
sebavazque <88560731+sebavazque@users.noreply.github.com>
2023-02-12 22:54:03 -0300

    app

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path')
-const http = require('http');
+
 
 app.use (express.json())
 
 // SDK de Mercado Pago
 const mercadopago = require ('mercadopago');
-const server = http.createServer(mercadopago);
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('port', process.env.PORT || 3001)
@@ -19,7 +19,7 @@ app.use(express.static(
   access_token: 'APP_USR-6623451607855904-111502-1f258ab308efb0fb26345a2912a3cfa5-672708410'})
 
           //routes
-          server.post('/payment', (req, res) => {
+          app.post('/payment', (req, res) => {
            
             let preference = {
               items: [
@@ -44,5 +44,5 @@ app.use(express.static(
         //     console.log(`Server on port ${app.get('port')}`)
         // })
 
-        server.listen(3001);
+        app.listen(3001);
         console.log("Server")
